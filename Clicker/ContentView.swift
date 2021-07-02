53cce8685c8225604de50e1b69f3af23551cf54a
James Erringham-Bruce <james@qcutapp.com>
2021-07-02 19:52:04 +0100

    Initial Commit

---
+++ Clicker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Clicker
+//
+//  Created by James Erringham-Bruce on 02/07/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
